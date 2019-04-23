fcf2e0011f1625b41e9a400073a26dd486eb922c
irenezhang <irenezhang@utexas.edu>
2019-04-23 16:52:02 -0500

    fixed agent out montor typo

--- lab3/tb/agent_out.sv
+++ lab3/tb/agent_out.sv
@@ -6,7 +6,7 @@ class agent_out extends uvm_agent;
     uvm_analysis_port #(uart2wb) aport;
     uvm_analysis_port #(uart_tx_frame) bport;
 
-    uart2wb_monitor uart2wb_montor_h;
+    uart2wb_monitor uart2wb_monitor_h;
     uart_monitor_out uart_monitor_out_h;
 
     function new(string name, uvm_component parent);
@@ -16,7 +16,7 @@ class agent_out extends uvm_agent;
     function void build_phase(uvm_phase phase);
         aport=new("aport",this);
         bport=new("bport",this);
-        uart2wb_montor_h=uart2wb_montor::type_id::create("uart2wb_montor_h",this);
+        uart2wb_monitor_h=uart2wb_monitor::type_id::create("uart2wb_monitor_h",this);
         uart_monitor_out_h=uart_monitor_out::type_id::create("uart_monitor_out_h",this);
     endfunction: build_phase
 
